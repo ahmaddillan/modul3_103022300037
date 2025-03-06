e379e3f77280c0737842b55502ad734e67f88eae
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-06 11:17:09 +0700

    calculator

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,77 @@ namespace modul3_103022300037
         {
             InitializeComponent();
         }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+           textBox1.Text += "+";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int col1 = Int32.Parse(textBox1.Text.Split('+')[0]);
+            int col2 = Int32.Parse(textBox1.Text.Split('+')[1]);
+            int col3 = Int32.Parse(textBox1.Text.Split('+')[2]);
+
+           
+             int has = col1 + col2+ col3;
+             textBox1.Text = has.ToString();
+            
+
+            
+            
+
+            
+        }
     }
 }
